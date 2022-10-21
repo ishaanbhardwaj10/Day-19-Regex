71a82bcd9e60f0dbae9ca92ad887fc4d80fa5f1a
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-21 11:06:06 +0530

    added UC-1 option to switch case in main method

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to User Registration Problem");
+            Console.WriteLine("Use case #1: First Name");
+
+            Console.Write("Please select a program to run from options above: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    string InputToValidate = "Ishaan";
+                    if (ValidateFirstName.validateString(InputToValidate))
+                    {
+                        Console.WriteLine("Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Please enter a valid number from given options");
+                    break;
+            }
+
+
+        }
+    }
+}
