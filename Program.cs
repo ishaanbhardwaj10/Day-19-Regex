06fb269914f2446fdc9907c24d742b2eae649d81
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-21 11:37:26 +0530

    added UC-4 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace RegexUserRegistration
             Console.WriteLine("Use case #1: First Name");
             Console.WriteLine("Use case #2: Last Name");
             Console.WriteLine("Use case #3: Email");
+            Console.WriteLine("Use case #4: Phone Numbers");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -49,6 +50,18 @@ namespace RegexUserRegistration
                     {
                         Console.WriteLine("Invalid");
                     }
+                    break;
+                case 4:
+                    string numberToValidate = "91 9876543210";
+                    if (ValidatePhoneNumber.validateString(numberToValidate))
+                    {
+                        Console.WriteLine("Valid");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid");
+                    }
+
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
